7d1431ad2ba2da4f34532fb9a552366bd1e60431
Jackgba <m.augusto.nd@gmail.com>
2019-06-09 15:50:19 -0300

    Update codigo do premio dat.c

--- codigo do premio dat.c
+++ codigo do premio dat.c
@@ -1,5 +1,5 @@
-// Esse código gera um número aleatorio dos números do arquivo PREMIOS.DAT
-// Está funcionando perfeitamente de maneira isolada, mas quando tento colocar no código principal sempre dá erro
+// Esse cÃ³digo gera um nÃºmero aleatorio dos nÃºmeros do arquivo PREMIOS.DAT
+// EstÃ¡ funcionando perfeitamente de maneira isolada, mas quando tento colocar no cÃ³digo principal sempre dÃ¡ erro
 
 #include <stdio.h>
 
@@ -15,7 +15,7 @@ int main(void)
    printf("Erro, nao foi possivel abrir o arquivo\n");
  else
  for(i=0;i<12;i++){ //cria um loop para armazenar cada valor de premio no vetor premio
-if ((fscanf(arq, "%f\n", &premio[i])) == 1 ) // leia a explicacao no final do código
+if ((fscanf(arq, "%f\n", &premio[i])) == 1 ) // leia a explicacao no final do cÃ³digo
     srand(time(NULL));
 	valor = premio[rand()%12];
 }
@@ -26,14 +26,13 @@ if ((fscanf(arq, "%f\n", &premio[i])) == 1 ) // leia a explicacao no final do có
  return 0;
 }
 
-// tentativa de colocar dentro de uma função:
+// tentativa de colocar dentro de uma funÃ§Ã£o:
 
 /*
 
 float premios()
 {
- char url[]="PREMIOS.dat",
-      nome[20];
+ char url[]="PREMIOS.dat";
  float premio[12], valor;
  int i;
  FILE *arq;
@@ -55,13 +54,13 @@ if ((fscanf(arq, "%f\n", &premio[i])) == 1 )
 */
 
 /*
-A função fscanf serve para transformar uma linha de um arquivo em uma variável podendo ter varios formatos.
-Foi a unica função que eu consegui fazer funcionar para ler esse arquivo como float
+A funÃ§Ã£o fscanf serve para transformar uma linha de um arquivo em uma variÃ¡vel podendo ter varios formatos.
+Foi a unica funÃ§Ã£o que eu consegui fazer funcionar para ler esse arquivo como float
 
-arq é o arquivo. %f é o tipo de dado da primeira linha e coluna do arquivo (se o arquivo fosse, por exemplo:
+arq Ã© o arquivo. %f Ã© o tipo de dado da primeira linha e coluna do arquivo (se o arquivo fosse, por exemplo:
 200 abc
-dava pra ler usando fscanf(arq,%f %s\n) e daria para criar um loop para ler várias linhas). premio[i] é
-o valor dos premios que serão armazenados. == 1 é para caso dê tudo certo na leitura (como se fosse um teste de erro).
+dava pra ler usando fscanf(arq,%f %s\n) e daria para criar um loop para ler vÃ¡rias linhas). premio[i] Ã©
+o valor dos premios que serÃ£o armazenados. == 1 Ã© para caso dÃª tudo certo na leitura (como se fosse um teste de erro).
 
-depois disso acho que dá para entender.
+depois disso acho que dÃ¡ para entender.
 */
